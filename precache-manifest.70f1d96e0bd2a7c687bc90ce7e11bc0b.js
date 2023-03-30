9845e59c055d7d65026369f5628fc62ddea90b31
Fernando Simoes <fsimoes99@gmail.com>
2023-03-30 23:07:48 +0100

    Updates

---
+++ precache-manifest.70f1d96e0bd2a7c687bc90ce7e11bc0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b7526f4bf740b2aadab3fe7abc32d8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "013ca7bc586af5389895",
+    "url": "/portfolio/static/css/2.3aee7458.chunk.css"
+  },
+  {
+    "revision": "6b2b4d7afe0e4c72e026",
+    "url": "/portfolio/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "013ca7bc586af5389895",
+    "url": "/portfolio/static/js/2.7dfa7678.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.7dfa7678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b2b4d7afe0e4c72e026",
+    "url": "/portfolio/static/js/main.b634e591.chunk.js"
+  },
+  {
+    "revision": "58d18144e62526203a32",
+    "url": "/portfolio/static/js/runtime-main.770b1f5e.js"
+  }
+]);
